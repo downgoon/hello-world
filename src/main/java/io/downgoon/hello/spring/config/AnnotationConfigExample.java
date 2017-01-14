a730c52de03965fd304eb0e0dc57ffe298131344
downgoon <downgoon@qq.com>
2017-01-14 16:52:27 +0800

    spring-config-c5-confmulti

--- src/main/java/io/downgoon/hello/spring/config/AnnotationConfigExample.java
+++ src/main/java/io/downgoon/hello/spring/config/AnnotationConfigExample.java
@@ -2,22 +2,19 @@ package io.downgoon.hello.spring.config;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
 
 public class AnnotationConfigExample {
 
 	public static void main(String[] args) throws Exception {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-        Employee wangyi = ctx.getBean("wangyi", Employee.class);
-        System.out.println(wangyi);
         
-        Employee zhangsan = ctx.getBean("zhangsan", Employee.class);
-        System.out.println(zhangsan);
+        Employee employee = ctx.getBean(Employee.class);
+        System.out.println(employee);
         
-        Employee chenliu = ctx.getBean("chenliu", Employee.class);
-        System.out.println(chenliu);
+        Environment env = ctx.getBean(Environment.class);
         
-        Employee env = ctx.getBean("env", Employee.class);
-        System.out.println(env);
+        System.out.println(env.getProperty("employee.age", Integer.class));
 	}
 	
 }
--- src/main/java/io/downgoon/hello/spring/config/AppConfig.java
+++ src/main/java/io/downgoon/hello/spring/config/AppConfig.java
@@ -1,61 +1,21 @@
 package io.downgoon.hello.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
-@PropertySource("classpath:/application.properties")
+@PropertySource({ "classpath:/application.properties", "classpath:/another.properties" })
 public class AppConfig {
-	
-	@Value("${employee.name}")
-	private String employeeName;
 
-	@Value("${employee.age}")
-	private Integer employeeAge;
-
-	@Bean(name = "chenliu")
-	public Employee employeeChenliu() {
-
-		Employee e = new Employee();
-		e.setName(employeeName);
-		e.setAge(employeeAge);
-		return e;
-	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeHolder() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-	
-	
 	@Autowired
 	private Environment env;
-	
-	@Bean(name = "env")
+
+	@Bean
 	public Employee employeeFromEnv() {
 		Employee e = new Employee();
 		e.setName(env.getProperty("employee.name"));
 		e.setAge(env.getProperty("employee.age", Integer.class));
-		return e;
-	}
-	
-
-	@Bean(name = "wangyi")
-	public Employee employee() {
-		Employee e = new Employee();
-		e.setName("wangyi");
-		e.setAge(28);
-		return e;
-	}
-
-	@Bean(name = "zhangsan")
-	public Employee employeeZhangsan() {
-		Employee e = new Employee();
-		e.setName("zhangsan");
-		e.setAge(37);
 		return e;
 	}
 
